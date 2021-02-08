75fe7a20b18360bfbca8750a5528e2211df77d80
Paul Esch-Laurent <herppfel@gmail.com>
2021-02-07 23:32:35 -0600

    feat: RSS feed

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 DIR="$(mktemp -d)"
 curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
 unzip "$DIR"/bic.zip
-docker build "$DIR" -t bic:ci
+docker build "$DIR"/bic-master -t bic:ci
 docker run --rm -v "$PWD":/src bic:ci
