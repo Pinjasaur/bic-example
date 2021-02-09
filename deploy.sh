a818c6787a34057c2c9605de08b3728a51c4543e
Paul Esch-Laurent <herppfel@gmail.com>
2021-02-08 23:52:41 -0600

    ci: can't cd to a file

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 DIR="$(mktemp -d)"
 curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
 unzip "$DIR"/bic.zip
-cd "$DIR"/bic.zip && docker build . -t bic:ci
+cd "$DIR"/bic-master && docker build . -t bic:ci
 docker run --rm -v "$GITHUB_WORKSPACE":/src bic:ci
