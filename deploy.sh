d522d28c4e2cde5ecbb0859d0078b2c5d4da43db
Paul Esch-Laurent <herppfel@gmail.com>
2021-02-08 23:55:30 -0600

    ci: unzip to specific location

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 DIR="$(mktemp -d)"
 curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
-unzip "$DIR"/bic.zip
-cd "$DIR"/bic-master && docker build . -t bic:ci
+unzip "$DIR"/bic.zip -d "$DIR"/bic
+docker build "$DIR"/bic -t bic:ci
 docker run --rm -v "$GITHUB_WORKSPACE":/src bic:ci
