8c8ecdc906f641948998a82ba52a3f0edcbed242
Paul Esch-Laurent <herppfel@gmail.com>
2021-02-08 23:56:25 -0600

    ci: gets unzipped to `bic-master`

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 DIR="$(mktemp -d)"
 curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
 unzip "$DIR"/bic.zip -d "$DIR"/bic
-docker build "$DIR"/bic -t bic:ci
+docker build "$DIR"/bic/bic-master -t bic:ci
 docker run --rm -v "$GITHUB_WORKSPACE":/src bic:ci
