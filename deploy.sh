296f4c7c7577f099bf26ee3bff9cefcd449e5259
Paul Esch-Laurent <herppfel@gmail.com>
2021-02-08 23:51:29 -0600

    ci: fix deploy.sh?

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 DIR="$(mktemp -d)"
 curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
 unzip "$DIR"/bic.zip
-docker build "$DIR"/bic-master -t bic:ci
-docker run --rm -v "$PWD":/src bic:ci
+cd "$DIR"/bic.zip && docker build . -t bic:ci
+docker run --rm -v "$GITHUB_WORKSPACE":/src bic:ci
