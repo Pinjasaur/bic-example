d926c019f99426321d565d4b2b04b6464d07d7d0
Paul Esch-Laurent <p@uly.io>
2021-02-10 00:59:13 -0600

    refactor: delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DIR="$(mktemp -d)"
-curl -sL https://github.com/Pinjasaur/bic/archive/master.zip -o "$DIR"/bic.zip
-unzip "$DIR"/bic.zip -d "$DIR"/bic
-docker build "$DIR"/bic/bic-master -t bic:ci
-docker run --rm -v "$GITHUB_WORKSPACE":/src bic:ci
